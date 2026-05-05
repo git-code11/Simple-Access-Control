423d601a464d5a404665cfc6326fc7988c0a3e64
Bolu <seiduboluwatife@gmail.com>
2026-05-05 05:18:06 +0100

    commented unsynthesised file

--- src/simple_access_control.v
+++ src/simple_access_control.v
@@ -1,56 +1,56 @@
-module simple_access_control (
-    input clk,
-    input rst_n,
-    input [3:0] row,
-    input [3:0] col,
-    output reg [2:0] rgb_out
-);
-  // FSM State Definitions
-  localparam ST_IDLE = 2'b00, ST_CHECK = 2'b01, ST_SUCCESS = 2'b10, ST_FAIL = 2'b11;
+// module simple_access_control (
+//     input clk,
+//     input rst_n,
+//     input [3:0] row,
+//     input [3:0] col,
+//     output reg [2:0] rgb_out
+// );
+//   // FSM State Definitions
+//   localparam ST_IDLE = 2'b00, ST_CHECK = 2'b01, ST_SUCCESS = 2'b10, ST_FAIL = 2'b11;
 
-  reg [1:0] current_state, next_state;
-  wire _unused = &{row, col, 1'b1};  // PIN WOULD STILL BE NEEDED
+//   reg [1:0] current_state, next_state;
+//   wire _unused = &{row, col, 1'b1};  // PIN WOULD STILL BE NEEDED
 
-  // Sub-modules
-  // KEYPAD_SCANNER
+//   // Sub-modules
+//   // KEYPAD_SCANNER
 
-  // DEBOUNCER
+//   // DEBOUNCER
 
-  // FSM State Transition
-  always @(posedge clk or negedge rst_n) begin
-    if (!rst_n) current_state <= ST_IDLE;
-    else current_state <= next_state;
-  end
+//   // FSM State Transition
+//   always @(posedge clk or negedge rst_n) begin
+//     if (!rst_n) current_state <= ST_IDLE;
+//     else current_state <= next_state;
+//   end
 
-  // Output and Next-State Logic
-  always @(current_state, negedge rst_n) begin
-    if (!rst_n) begin
-      rgb_out <= 3'b000;
-      next_state <= ST_IDLE;
-    end else begin
-      case (current_state)
-        ST_IDLE: begin
-          rgb_out <= 3'b100;
-        end
+//   // Output and Next-State Logic
+//   always @(current_state, negedge rst_n) begin
+//     if (!rst_n) begin
+//       rgb_out <= 3'b000;
+//       next_state <= ST_IDLE;
+//     end else begin
+//       case (current_state)
+//         ST_IDLE: begin
+//           rgb_out <= 3'b100;
+//         end
 
-        ST_CHECK: begin
-          rgb_out <= 3'b100;
-          next_state <= ST_CHECK;
-        end
+//         ST_CHECK: begin
+//           rgb_out <= 3'b100;
+//           next_state <= ST_CHECK;
+//         end
 
-        ST_SUCCESS: begin
-          rgb_out <= 3'b010;
-          next_state <= ST_SUCCESS;  // Stay until reset
-        end
+//         ST_SUCCESS: begin
+//           rgb_out <= 3'b010;
+//           next_state <= ST_SUCCESS;  // Stay until reset
+//         end
 
-        ST_FAIL: begin
-          rgb_out <= 3'b100;
-          next_state <= ST_FAIL;  // Stay until reset
-        end
+//         ST_FAIL: begin
+//           rgb_out <= 3'b100;
+//           next_state <= ST_FAIL;  // Stay until reset
+//         end
 
-        default: next_state <= ST_IDLE;  // Failsafe reset
-      endcase
-    end
-  end
-endmodule
+//         default: next_state <= ST_IDLE;  // Failsafe reset
+//       endcase
+//     end
+//   end
+// endmodule
 
